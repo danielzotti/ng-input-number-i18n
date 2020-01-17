878c321e2a4a96ed2b1e8029520447490166c2c0
Daniel Zotti <daniel.zotti87@gmail.com>
2020-01-17 09:36:39 +0100

    Added library project

---
+++ projects/ng-input-i18n/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-input-i18n
+ */
+
+export * from './lib/ng-input-i18n.service';
+export * from './lib/ng-input-i18n.component';
+export * from './lib/ng-input-i18n.module';
