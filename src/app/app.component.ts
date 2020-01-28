9e1b9dc822c92525b97bdd485a4128f75bf7de09
Daniel Zotti <daniel.zotti@promoscience.com>
2020-01-28 16:26:14 +0100

    Fix grafici

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { getLocaleId, getLocaleNumberSymbol } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   values = [
